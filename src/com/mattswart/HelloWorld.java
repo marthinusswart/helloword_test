6f75627c7c28e24ca7977be39d820a1c9ba68316
marthinusswart <matt.swart@icloud.com>
2024-02-17 17:52:47 +1100

    first commit

---
+++ src/com/mattswart/HelloWorld.java
@@ -0,0 +1,9 @@
+package com.mattswart;
+/**
+ * @author <a href="mailto:matt.swart@icloud.com">Matt Swart</a>
+ */
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+ }
